86dd54d3d7085f69cd6651d3806ba0d78842b2ab
munishsethi777 <munishsethi777@gmail.com>
2018-10-05 12:18:44 +0530

    booking ui

--- index.php
+++ index.php
@@ -16,12 +16,80 @@
 							SKY DINING<small> select your bookings</small>
 						</h5>
 					</div>
-					<div class="ibox-content mainDiv">
+					<div style="margin-top:10px">
+                    	<div class="col-lg-8">
+                       		<div class="row ibox-content" style="font-weight: bold">
+	                       		<div class="col-lg-2">
+	                       			Date
+	                       		</div>
+	                       		<div class="col-lg-2">
+	                       			Slot Time
+	                       		</div>
+	                       		<div class="col-lg-3">
+	                       			Fare
+	                       		</div>
+	                       		<div class="col-lg-3">
+	                       			Seats Available
+	                       		</div>
+	                       		<div class="col-lg-2">
+	                       			Action
+	                       		</div>
+	                       	</div>
+                       		
+                       		
+                       		<div class="row ibox-content">
+	                       		<div class="col-lg-2">
+	                       			17-09-2018<br>
+	                       			<small class="text-muted">Monday</small>
+	                       		</div>
+	                       		<div class="col-lg-2">
+	                       			7:30 PM<br>
+	                       			<small class="text-muted">Session1</small>
+	                       		</div>
+	                       		<div class="col-lg-3">
+	                       			Rs. 2000 (Veg) <br>
+	                       			Rs. 2800 (N.Veg)
+	                       		</div>
+	                       		<div class="col-lg-3">
+	                       			<div class="progress progress-mini">
+                                    	<div style="width: 100%;" class="progress-bar"></div>
+                                    </div>
+	                       			<small class="text-muted">8 Seats</small>
+	                       		</div>
+	                       		<div class="col-lg-2">
+	                       			<button class="btn btn-primary">BOOK NOW</button>
+	                       		</div>
+                       		</div>
+                       		<div class="row ibox-content">
+	                       		<div class="col-lg-2">
+	                       			17-09-2018<br>
+	                       			<small class="text-muted">Monday</small>
+	                       		</div>
+	                       		<div class="col-lg-2">
+	                       			7:30 PM<br>
+	                       			<small class="text-muted">Session1</small>
+	                       		</div>
+	                       		<div class="col-lg-3">
+	                       			Rs. 2000 (Veg) <br>
+	                       			Rs. 2800 (N.Veg)
+	                       		</div>
+	                       		<div class="col-lg-3">
+	                       			<div class="progress progress-mini">
+                                    	<div style="width: 50%;" class="progress-bar"></div>
+                                    </div>
+	                       			<small class="text-muted">4 Seats</small>
+	                       		</div>
+	                       		<div class="col-lg-2">
+	                       			<button class="btn btn-primary">BOOK NOW</button>
+	                       		</div>
+                       		</div>
+                       		
+                    	</div>
+                    	<div class="col-lg-4">
                        
+                    	</div>
                     </div>
-                    <div class="ibox-content mainDiv">
-                       
-                    </div>
+                    
 				</div>
 			</div>
 		</div>
