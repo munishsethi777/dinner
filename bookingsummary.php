6642a25fb599e245400106632c555e1120038896
munishsethi777 <munishsethi777@gmail.com>
2018-10-08 12:16:05 +0530

    new paged

---
+++ bookingsummary.php
@@ -0,0 +1,193 @@
+<?php ?>
+
+<html>
+<head>
+<title>Booking</title>
+<?include "ScriptsInclude.php"?>
+<style>
+	.file-box{
+		width:33%;
+	}
+</style>
+</head>
+<body>
+ <div id="wrapper">
+	<div class="wrapper wrapper-content animated fadeIn">
+       <div class="row">
+			<div class="col-lg-12">
+				<div class="ibox float-e-margins ">
+					<div style="margin-top:10px">
+                    	<div class="col-lg-8">
+                    		<div class="ibox-content">
+	                       		<div class="row text-center" style="margin-bottom:10px">
+	                       			<h1 style="padding-bottom:10px">
+											MENU
+									</h1>
+									<button class="btn btn-primary btn-rounded">VEG</button>
+									<button class="btn btn-primary btn-rounded">NON VEG</button>
+	                       		</div>
+		                       		<div class="row">
+		                       			<div class="file-box">
+			                                <div class="file">
+		                                        <div class="icon">
+		                                            <i class="fa fa-file"></i>
+		                                        </div>
+		                                        <div class="file-name">
+		                                            <p class="text-success">ITEM NAME</p>
+		                                            <small>Lorem ipsum dolor sit amet, 
+		                                            consectetur adipiscing elit, sed do eiusmod tempor 
+		                                            incididunt ut labore et 
+		                                            dolore magna aliqua.</small>
+		                                        </div>
+			                                </div>
+		                            	</div>
+		                            	
+		                            	<div class="file-box">
+			                                <div class="file">
+		                                        <div class="icon">
+		                                            <i class="fa fa-file"></i>
+		                                        </div>
+		                                        <div class="file-name">
+		                                            <p class="text-success">ITEM NAME</p>
+		                                            <small>Lorem ipsum dolor sit amet, 
+		                                            consectetur adipiscing elit, sed do eiusmod tempor 
+		                                            incididunt ut labore et 
+		                                            dolore magna aliqua.</small>
+		                                        </div>
+			                                </div>
+		                            	</div>
+		                            	
+		                            	<div class="file-box">
+			                                <div class="file">
+		                                        <div class="icon">
+		                                            <i class="fa fa-file"></i>
+		                                        </div>
+		                                        <div class="file-name">
+		                                            <p class="text-success">ITEM NAME</p>
+		                                            <small>Lorem ipsum dolor sit amet, 
+		                                            consectetur adipiscing elit, sed do eiusmod tempor 
+		                                            incididunt ut labore et 
+		                                            dolore magna aliqua.</small>
+		                                        </div>
+			                                </div>
+		                            	</div>
+		                            	
+		                            	<div class="file-box">
+			                                <div class="file">
+		                                        <div class="icon">
+		                                            <i class="fa fa-file"></i>
+		                                        </div>
+		                                        <div class="file-name">
+		                                            <p class="text-success">ITEM NAME</p>
+		                                            <small>Lorem ipsum dolor sit amet, 
+		                                            consectetur adipiscing elit, sed do eiusmod tempor 
+		                                            incididunt ut labore et 
+		                                            dolore magna aliqua.</small>
+		                                        </div>
+			                                </div>
+		                            	</div>
+		                            	
+		                            	<div class="file-box">
+			                                <div class="file">
+		                                        <div class="icon">
+		                                            <i class="fa fa-file"></i>
+		                                        </div>
+		                                        <div class="file-name">
+		                                            <p class="text-success">ITEM NAME</p>
+		                                            <small>Lorem ipsum dolor sit amet, 
+		                                            consectetur adipiscing elit, sed do eiusmod tempor 
+		                                            incididunt ut labore et 
+		                                            dolore magna aliqua.</small>
+		                                        </div>
+			                                </div>
+		                            	</div>
+		                            	
+		                            	<div class="file-box">
+		                                <div class="file">
+	                                        <div class="icon">
+	                                            <i class="fa fa-file"></i>
+	                                        </div>
+	                                        <div class="file-name">
+	                                            <p class="text-success">ITEM NAME</p>	
+	                                            <small>Lorem ipsum dolor sit amet, 
+	                                            consectetur adipiscing elit, sed do eiusmod tempor 
+	                                            incididunt ut labore et 
+	                                            dolore magna aliqua.</small>
+	                                        </div>
+		                                </div>
+	                            	</div>
+	                       			</div>
+                       			</div>
+                    	</div>
+                    	<div class="col-lg-4">
+                    		<div class="ibox-content">
+	                       			<h3>BOOKING SUMMARY</h3>
+	                       			<div class="row" style="margin-bottom:5px">
+	                       				<div class="col-md-8">SLOT 7:30PM - 9:30PM</div>
+	                       				<div class="col-md-4 text-right">Rs 10,500.00</div>
+	                       			</div>
+	                       			<div class="row">	
+	                       				<div class="col-lg-8">
+	                       					<small class="text-muted">
+	                       						2 VEG - 2 X 4500 <br>
+	                       						2 N.VEG - 2 X 4500 M<br>
+	                       					</small>
+	                       				</div>
+	                       				<div class="col-lg-4 text-right"></div>
+	                       			</div>
+	                       			
+	                       			<div class="row m-b-sm">	
+	                       				<div class="col-lg-8">
+	                       					<small class="text-muted">
+	                       						Internet Handling Fees
+	                       					</small>
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Rs 750.00</div>
+	                       			</div>
+	                       			<div class="row bg-muted p-h-sm">	
+	                       				<div class="col-lg-8">
+	                       					Sub Total
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Rs 10,750.00</div>
+	                       			</div>
+	                       			
+	                       			<div class="row bg-success p-h-sm text-uppercase font-bold">	
+	                       				<div class="col-lg-8">
+	                       					AMOUNT PAYABLE
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Rs 10,750.00</div>
+	                       			</div>
+	                       			
+	                       			<div class="row bg-primary m-t-lg p-h-sm font-bold">	
+	                       				<div class="col-lg-8">
+	                       					TOTAL Rs 10,750.00
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Proceed</div>
+	                       			</div>
+	                       			<div class="row m-t-sm text-center">	
+	                       				<small class="text-muted">
+		                       				You can cancel the tickets upto 4 hours before the show. <br>
+		                       				Refunds will be done according to <a href="#">Cancellation Policy</a>
+	                       				</small>
+	                       			</div>
+	                       	</div>
+                       	</div>
+                    </div>
+                    
+				</div>
+			</div>
+		</div>
+   	</div>
+ </div>
+</body>
+</html>
+<script src="scripts/FormValidators/FormValidators.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){ 
+	
+});
+
+
+
+
+</script> 
---
+++ checkout.php
@@ -0,0 +1,149 @@
+<?php ?>
+
+<html>
+<head>
+<title>Booking</title>
+<?include "ScriptsInclude.php"?>
+<style>
+	.file-box{
+		width:33%;
+	}
+</style>
+</head>
+<body>
+ <div id="wrapper">
+	<div class="wrapper wrapper-content animated fadeIn">
+       <div class="row">
+			<div class="col-lg-12">
+				<div class="ibox float-e-margins ">
+					<div style="margin-top:10px">
+                    	<div class="col-lg-8">
+                    		<div class="ibox border-bottom">
+			                    <div class="ibox-title bg-success">
+			                        <a class="collapse-link" style="float:left;margin-right:6px;color:white">
+		                                <i class="fa fa-chevron-up"></i>
+		                            </a>
+			                        <h5>Share Your Contact Details</h5> 
+			                    </div>
+			                    <div class="ibox-content">
+									<form role="form" class="form-inline">
+		                                <div class="form-group">
+		                                    <input type="text" placeholder="Enter Full Name" id="fullName" class="form-control">
+		                                </div>
+		                                <div class="form-group">
+		                                    <input type="text" placeholder="Enter Mobile no" id="mobile" class="form-control">
+		                                </div>
+		                                <div class="form-group">
+		                                    <input type="email" placeholder="Enter Email id" id="email" class="form-control">
+		                                </div>
+		                                <div class="form-group">
+		                                    <button class="btn btn-primary" type="submit" style="margin-top:5px">Continue</button>
+		                                </div>
+		                            </form>			                        
+                				</div>
+                    		</div>
+                    		
+                    		<div class="ibox border-bottom">
+			                    <div class="ibox-title bg-success">
+			                        <a class="collapse-link" style="float:left;margin-right:6px;color:white">
+		                                <i class="fa fa-chevron-down"></i>
+		                            </a>
+			                        <h5>Payment Options</h5> 
+			                    </div>
+			                    <div class="ibox-content" style="display:none">
+									<form role="form" class="form-inline">
+		                                <div class="form-group">
+		                                    <input type="text" placeholder="Enter Full Name" id="fullName" class="form-control">
+		                                </div>
+		                                <div class="form-group">
+		                                    <input type="text" placeholder="Enter Mobile no" id="mobile" class="form-control">
+		                                </div>
+		                                <div class="form-group">
+		                                    <input type="email" placeholder="Enter Email id" id="email" class="form-control">
+		                                </div>
+		                                <div class="form-group">
+		                                    <button class="btn btn-primary" type="submit" style="margin-top:5px">Continue</button>
+		                                </div>
+		                            </form>			                        
+                				</div>
+                    		</div>
+                    		
+                    		
+                    	</div>
+                    	
+                    	
+                    	
+                    	
+                    	
+                    	<div class="col-lg-4">
+                    		<div class="ibox-content">
+	                       			<h3>BOOKING SUMMARY</h3>
+	                       			<div class="row" style="margin-bottom:5px">
+	                       				<div class="col-md-8">SLOT 7:30PM - 9:30PM</div>
+	                       				<div class="col-md-4 text-right">Rs 10,500.00</div>
+	                       			</div>
+	                       			<div class="row">	
+	                       				<div class="col-lg-8">
+	                       					<small class="text-muted">
+	                       						2 VEG - 2 X 4500 <br>
+	                       						2 N.VEG - 2 X 4500 M<br>
+	                       					</small>
+	                       				</div>
+	                       				<div class="col-lg-4 text-right"></div>
+	                       			</div>
+	                       			
+	                       			<div class="row m-b-sm">	
+	                       				<div class="col-lg-8">
+	                       					<small class="text-muted">
+	                       						Internet Handling Fees
+	                       					</small>
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Rs 750.00</div>
+	                       			</div>
+	                       			<div class="row bg-muted p-h-sm">	
+	                       				<div class="col-lg-8">
+	                       					Sub Total
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Rs 10,750.00</div>
+	                       			</div>
+	                       			
+	                       			<div class="row bg-success p-h-sm text-uppercase font-bold">	
+	                       				<div class="col-lg-8">
+	                       					AMOUNT PAYABLE
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Rs 10,750.00</div>
+	                       			</div>
+	                       			
+	                       			<div class="row bg-primary m-t-lg p-h-sm font-bold">	
+	                       				<div class="col-lg-8">
+	                       					TOTAL Rs 10,750.00
+	                       				</div>
+	                       				<div class="col-lg-4 text-right">Proceed</div>
+	                       			</div>
+	                       			<div class="row m-t-sm text-center">	
+	                       				<small class="text-muted">
+		                       				You can cancel the tickets upto 4 hours before the show. <br>
+		                       				Refunds will be done according to <a href="#">Cancellation Policy</a>
+	                       				</small>
+	                       			</div>
+	                       	</div>
+                       	</div>
+                    </div>
+                    
+				</div>
+			</div>
+		</div>
+   	</div>
+ </div>
+</body>
+</html>
+<script src="scripts/FormValidators/FormValidators.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){ 
+	
+});
+
+
+
+
+</script> 
