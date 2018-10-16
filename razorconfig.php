fd68cd6a23cbb0852d8ce192d6d4d54e087c2d23
munishsethi777 <munishsethi777@gmail.com>
2018-10-16 11:24:30 +0530

    new files

---
+++ razorconfig.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_live_zZ6x7CvsASE4M3';
+$keySecret = 'UPThb1jCZh0eKqyxTJSsAEjy';
+$displayCurrency = 'Rs.';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
