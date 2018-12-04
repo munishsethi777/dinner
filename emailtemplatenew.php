3fa949e259953ce1e64cc3d23398abc741ee0ae8
munishsethi777 <munishsethi777@gmail.com>
2018-12-04 12:14:48 +0530

    new email template

---
+++ emailtemplatenew.php
@@ -0,0 +1,205 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="1https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+	<div
+		style="background-color: grey; width: 100%; color: #676a6c; font-family: open sans, Helvetica Neue, Helvetica, Arial, sans-serif">
+		<div
+			style="background-color: white; margin: auto; max-width: 600px; padding: 0px 15px 0px 15px">
+			<div
+				style="padding: 15px; background-color: #DAA520; color: white; margin: 0px -15px 0px -15px;">
+				<h1 style="margin-top: 0px; margin-bottom: 20px;">
+					<img
+						src="https://1ceipl3l02ez252khj15lgak-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/logo-white-small.png"
+						style="float: left;">
+				</h1>
+				<p align="right" style="margin: 0px;">
+					<a href="https://www.flydining.com/"
+						style="color: #fff; text-decoration: none;">www.flydining.com</a>
+				</p>
+			</div>
+			<div style="margin-top: 20px;display:flex;padding:15px;">
+				<div style="max-width: 25%">
+					<img
+						src="https://static1.squarespace.com/static/597aa449d482e9e56c97117c/t/59e6cc69a9db0951565bdd72/1508297874678/green+checkmark+Large.png?format=300w"
+						width="100%">
+				</div>
+				<div style="padding:20px 15px;max-width: 75%">
+					<p style="margin: 0px; font-size: 24px;">Thank you for your
+						booking!</p>
+					<p
+						style="font-size: 24px; font-weight: bold; margin: 0px; color: #000;">Order
+						Id: 125C4E</p>
+				</div>
+			</div>
+			<div style="margin: 20px 0 0;display:flex">
+				<div style="border-right: 1px solid #f1f1f1;max-width:50%;padding:0px 15px;">
+					<h2 style="color: #000;">Flydining</h2>
+					<p style="margin: 0px;">Sky Lounge, 24/10, M M Reddy Layout,
+						Mariyanpally, Beside Nagavara Lake, Kempapura Main Road, Hebbal,
+						Bengaluru, Karnataka - 560024, India</p>
+					<p style="margin: 0px;">
+						<span style="font-weight: bold;">GST#:</span> 29ADHFS4111J1ZY
+					</p>
+					<br>
+					<p>
+						to,<br> <span style="font-weight: bold;">Kishan</span><br>kishan@gmail.com<br>9080232610
+					</p>
+				</div>
+				<div style="margin-bottom: 20px;max-width:50%;padding:0px 15px;">
+					<p
+						style="font-weight: bold; font-size: 21px; color: #000; margin-bottom: 0px;">2x
+						Mocktail and Complementary Snacks</p>
+					<p style="font-size: 21px; margin-bottom: 0px;">All Veg</p>
+					<br>
+					<p style="margin: 0px;">Venue:</p>
+					<a
+						href="https://www.google.co.in/maps/dir//13.0424729,77.6135216/13.0464791,77.6115609/Fly+Dining,+Nagwara+Backside+Lumbini+Garden,+Bengaluru,+Karnataka+560024/@13.0436273,77.6099459,1255m/data=!3m1!1e3!4m11!4m10!1m0!1m0!1m0!1m5!1m1!1s0x3bae173340c3be35:0x19af15ffe521aced!2m2!1d77.6110824!2d13.0471114!3e0">Get
+						direction</a><br> <br>
+					<p style="margin: 0px;">Time:</p>
+					<p style="font-weight: bold; color: #000; font-size: 18px;">3pm to
+						5pm | Nov 8, 2018</p>
+				</div>
+			</div>
+
+
+			
+
+			<div style="background: #D8F5F5; margin: 0px; padding: 15px;display:flex">
+				<p style="margin: 0px;">
+					Dear Kishan,<br> <br> Thank you for choosing Flydining. We will do
+					our best to make this experience phenomenal for you. We look
+					forward to see you.
+				</p>
+			</div>
+			
+			<div style="margin: 0; background: #f1f1f1; padding: 20px 20;display:flex">
+				<div class="col-sm-12">
+					<p
+						style="margin: 0 0 10px 0; font-weight: bold; border-bottom: 1px silver solid;">Order
+						Summary</p>
+					<table style="text-align:left;font-size:12px;vertical-align:top;border-bottom:1px silver solid;">
+						<tr>
+							<th width="30%" style="text-align:left;vertical-align:top">Item</th>
+							<th width="15%" style="text-align:right;vertical-align:top">Rate</th>
+							<th width="10%" style="text-align:right;vertical-align:top">Qty</th>
+							<th width="15%" style="text-align:right;vertical-align:top">Gross Amt</th>
+							<th width="15%" style="text-align:right;vertical-align:top">CGST<br>(+2.5%)</th>
+							<th width="15%" style="text-align:right;vertical-align:top">SGST<br>(+2.5%)</th>
+						</tr>
+						<tr>
+							<td width="30%" style="text-align:left;vertical-align:top">Mocktail and Complementary Snacks</td>
+							<td width="15%" style="text-align:right;vertical-align:top">4760.50</td>
+							<td width="10%" style="text-align:right;vertical-align:top">2</td>
+							<td width="15%" style="text-align:right;vertical-align:top">9521.00</td>
+							<td width="15%" style="text-align:right;vertical-align:top">238.50</td>
+							<td width="15%" style="text-align:right;vertical-align:top;padding-bottom:30px">238.50</td>
+						</tr>
+						<tr>
+							<td width="30%" style="text-align:left;vertical-align:top">Mocktail and Complementary Snacks</td>
+							<td width="15%" style="text-align:right;vertical-align:top">4760.50</td>
+							<td width="10%" style="text-align:right;vertical-align:top">2</td>
+							<td width="15%" style="text-align:right;vertical-align:top">9521.00</td>
+							<td width="15%" style="text-align:right;vertical-align:top">238.50</td>
+							<td width="15%" style="text-align:right;vertical-align:top;padding-bottom:30px">238.50</td>
+						</tr>
+					</table>		
+					<div style="display:flex;width:100%">
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:left">
+							<p
+								style="font-weight: bold; color: #000; font-size: 21px; margin: 0px;">Total
+								Amount</p>
+						</div>
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:right;">
+							<p
+								style="font-weight: bold; color: #000; font-size: 21px; text-align: right; margin: 0px;">Rs.
+								9998.00/-</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div style="margin: 10px 0 0;">
+				<p><h4 style="display:block">Important Instructions</h4></p>
+				<ul style="font-size: 12px;">
+					<li>Be on time- at least a minimum of 60 minutes before your table
+						reservation. If you are late for your reservation, no refund will
+						be given and no rescheduling will be allowed.</li>
+					<li>Dress comfortably. Avoid wearing loose shoes.</li>
+					<li>Do not bring large handbags or umbrellas etc with you. We have
+						limited storage/locker space.</li>
+					<li>Do not make other plans for the night. Depending on the
+						weather, we might delay your experience, to ensure you enjoy the
+						beautiful Flydining In India.</li>
+					<li>Do not bring valuables to the experience.</li>
+					<li>Please bring along your Identification Card/ Passport for
+						verification purpose.</li>
+					<li>Remember, this experience is strictly for individuals that are
+						13 years old and above, with a minimum height 145cm and a maximum
+						weight of 150kg. Anyone below the age of 18 MUST be accompanied by
+						an adult or guardian, or not be allowed to board. Pregnant women
+						are also not allowed to experience Flydining In India.</li>
+					<li>The ticket is NOT-TRANSFERABLE in anyway and any changes of
+						Dinner(s) name/information/menu is not allowed.</li>
+					<li>If you are lost or need direction, please call
+						+91-769-81-81-000. Kindly take note that we are unable to delay
+						the experience for you if you call or message us on this number so
+						please do not be late.</li>
+				</ul>
+			</div>
+			<p align="center" style="color: #ff0000;">
+				<span style="font-size: 21px;">Session's pictures will be updated on
+					the end of the day,</span><br> <a
+					href="https://photos.google.com/share/AF1QipNEwpOWZiYOV59xaV0_XPf0bbiukFcneeccG8yJ_uepL75c92w3TGsL-krGbQpWZw?key=THk0ZlZhR2NyclBiQWVyU013OFAzWGcwcS03SkNB">Click
+					here</a> <span style="color: #000;">to view images. </span><a
+					href="https://www.flydining.com/terms-conditions/">Terms &amp;
+					Conditions</a>
+			</p>
+
+			<div>
+				<div align="center">
+					<a
+						href="https://play.google.com/store/apps/details?id=com.ni.FlyDining"><img
+						src="https://1ceipl3l02ez252khj15lgak-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/global-playstore.png"
+						width="30%" style="margin: 0 0 10px;"></a>&nbsp;<a
+						href="https://www.apple.com/in/ios/app-store/"><img
+						src="https://1ceipl3l02ez252khj15lgak-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/Untitled-1.png"
+						width="30%" style="margin: 0 0 10px;"></a>
+				</div>
+			</div>
+
+			<div style="margin: 0 0 10px;">
+				<div align="center">
+					<p style="margin: 20px 0 0">
+						Follow us on: <a href="https://www.facebook.com/flydining"><img
+							src="https://ci4.googleusercontent.com/proxy/FwzcgIsBuYYL2YTPtfI17st2JQb_oFfserSYPNYEOqY35orlheD8cNvdNvgNzQsgGwSWzuBV5maD9eE8vsEsiH46fO3YshqOF8zTb0FWKJlDorF7=s0-d-e1-ft#https://in.bmscdn.com/mailers/images/160720bmsreview/facebook.png"></a><a
+							href="https://twitter.com/FlyDining"><img
+							src="https://ci4.googleusercontent.com/proxy/jcI7YLl108K8e8tJyok7swAAE_aD5vFR0LOGWfm8Nc4DR1fw3X9dkzFnUH_GtyZUMdl8Fjzsa3Y6q_EsJbBzVvu3CMsBNmXxO7SyKKvTXdf9Gas=s0-d-e1-ft#https://in.bmscdn.com/mailers/images/160720bmsreview/twitter.png"></a><a
+							href="https://www.youtube.com/channel/UCtmt1xDuJ7_chvSi2jVGiBA"><img
+							src="https://ci6.googleusercontent.com/proxy/hzZdxXsakGn7dEyoV3EM33eIpl_IpLrvpENCm3EEQ9y9As_CUFjK-yQj3u1sYsyR3KEA8Zyi-wQD2hF5LFwin6FoDrlaNmaghtEfBg9FvelnBJI=s0-d-e1-ft#https://in.bmscdn.com/mailers/images/160720bmsreview/youtube.png"
+							style="padding-left: 5px;"></a><a
+							href="https://www.instagram.com/fly.dining/"><img
+							src="https://ci3.googleusercontent.com/proxy/hC6s2VDAbzueVXNl2_3gaIwpjdNwR9JBIU0vdpBrHiln9bzJP7yxv8ACczjzHZcnEiKzH037w_sBCM-YYok8vdYByCLBr4YYq1fNSpCBdBYnjzzFYA=s0-d-e1-ft#https://in.bmscdn.com/mailers/images/160720bmsreview/instagram.png"
+							style="padding-left: 5px;"></a>
+					</p>
+				</div>
+			</div>
+
+
+			<p
+				style="background: #DAA520; margin-top: 1 0px; color: #000; padding: 10px;"
+				align="center">
+				For any further assistance, mail to blr@flydining.com<br>or call:<b>769
+					81 81 333, 8130 540 906</b>
+			</p>
+
+
+
+
+
+		</div>
+	</div>
+</body>
+</html>
