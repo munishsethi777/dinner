a3da50893556120bc419b64a131fb785b53e026d
munishsethi777 <munishsethi777@gmail.com>
2019-01-07 10:45:55 +0530

    header changes

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <?php ?>
 <div class="row">
-<div style="margin:15px">
-<a href="https://www.flydining.com"><img src="images/logo.png"></img></a>
-</div>
+	<div class="col-xs-6 p-sm">
+		<a href="https://www.flydining.com"><img src="images/logo.png"></img></a>
+	</div>
+	<div class="col-xs-6 text-right">
+		<h3><i class="fa fa-phone" aria-hidden="true"></i> Call 084484 44960</h3>
+	</div>
 </div>
