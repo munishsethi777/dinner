4e89c1c4a48d04a2183e98f27148854be698aef8
bgaheer <baljeetgaheer@gmail.com>
2019-01-18 10:21:01 +0530

    changes

---
+++ phoneInclude.php
@@ -0,0 +1,6 @@
+<?php ?>
+<div class="callfloatbutton mg-t-lg" style="z-index: 1;
+							position: fixed;
+							right:20px;
+							top:300px;"><a href="tel:918130540906"> <img src="images/phone.png" height="50px"></a></div>
+							</div>
