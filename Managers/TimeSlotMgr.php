dee9686ccfa77f135846a992cf997a64f7549b5b
bgaheer <baljeetgaheer@gmail.com>
2019-01-24 15:11:55 +0530

    changes

--- Managers/TimeSlotMgr.php
+++ Managers/TimeSlotMgr.php
@@ -227,6 +227,12 @@ inner JOIN menutimeslots on timeslots.seq = menutimeslots.timeslotsseq inner joi
 		return $timeSlots;
 	}
 	
+	public function getTimeSlotSeqsForNotification(){
+		$query = "select timeslots.seq from timeslots where seq not in (SELECT timeslotseq from notifications where notifications.senton = CURRENT_DATE ) and timeslots.bookingavailabletill  < DATE_FORMAT( NOW() , '%H:%i' ) ";
+		$timeSlotsSeqs = self::$dataStore->executeQuery($query);
+		return $timeSlotsSeqs;
+	}
+	
 	
 
 }
